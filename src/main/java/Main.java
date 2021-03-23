bca0ca6350fda7eea4c33399cf57b0126838cbe1
Kamila Nigmetzianova <kamila-nigmet@mail.ru>
2021-03-23 20:52:45 +0300

    Homework 3: added lemmas indexing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,10 @@ import java.util.*;
     Обработка и выделение токенов (включая лемматизацию) для выкаченных страниц из папки /downloads
  */
 public class Main {
-    private static final String lemmasPath = "lemmas.txt";
-
+    private static final String LEMMAS_PATH = "lemmas.txt";
+    private static final String INDEXES_PATH = "invertedIndexes.txt";
+    private static Map<String, List<String>> lemmasIndexes = new TreeMap<String, List<String>>(String.CASE_INSENSITIVE_ORDER);
+    private static Map<String, List<String>> uniqueLemmas = new TreeMap<String, List<String>>(String.CASE_INSENSITIVE_ORDER);
 
     public static void main(String[] args) {
         tokenize();
@@ -20,7 +22,6 @@ public class Main {
         File[] listOfFiles = folder.listFiles();
 
         StanfordLemmatizer lemmatizer = new StanfordLemmatizer();
-        Map<String, List<String>> uniqueLemmas = new TreeMap<String, List<String>>(String.CASE_INSENSITIVE_ORDER);
 
         if (listOfFiles == null) {
             System.out.println("Ensure /downloads folder is not empty");
@@ -29,26 +30,50 @@ public class Main {
         for (File file : listOfFiles) {
             if (file.isFile()) {
                 System.out.println("File " + file.getName() + " is processing");
+                String fileNumber = file.getName().split(".txt")[0];
                 HashMap<String, String> lemmas = lemmatizer.lemmatize(IOHelper.readFromFile(file));
                 for (Map.Entry<String, String> entry : lemmas.entrySet()) {
-                    if (!uniqueLemmas.containsKey(entry.getValue())) {
-                        uniqueLemmas.put(entry.getValue(), new ArrayList<String>());
-                    }
-                    if (!uniqueLemmas.get(entry.getValue()).contains(entry.getKey())) {
-                        uniqueLemmas.get(entry.getValue()).add(entry.getKey());
-                    }
+                    filterUniqueIndexes(fileNumber, entry);
+                    filterUniqueLemmas(entry);
                 }
                 System.out.println("File " + file.getName() + " processing ended");
             }
         }
 
-        uniqueLemmas = new TreeMap<String, List<String>>(uniqueLemmas);
-        for (Map.Entry<String, List<String>> entry : uniqueLemmas.entrySet()) {
+        writeInfoForEachLemmaToFile(uniqueLemmas, LEMMAS_PATH);
+        writeInfoForEachLemmaToFile(lemmasIndexes, INDEXES_PATH);
+    }
+
+    private static void filterUniqueLemmas(Map.Entry<String, String> entry) {
+        //если такая лемма еще не встречалась, завести для нее пустой список токенов
+        if (!uniqueLemmas.containsKey(entry.getValue())) {
+            uniqueLemmas.put(entry.getValue(), new ArrayList<String>());
+        }
+        //если такая лемма уже есть, но токен еще не встречался, добавить токен в ее список
+        if (!uniqueLemmas.get(entry.getValue()).contains(entry.getKey())) {
+            uniqueLemmas.get(entry.getValue()).add(entry.getKey());
+        }
+    }
+
+    private static void filterUniqueIndexes(String fileNumber, Map.Entry<String, String> entry) {
+        //если такая лемма еще не встречалась, завести для нее пустой список индексов
+        if (!lemmasIndexes.containsKey(entry.getValue())) {
+            lemmasIndexes.put(entry.getValue(), new ArrayList<String>());
+        }
+        //если такая лемма уже есть, но индекс файла еще не встречался, добавить индекс в ее список
+        if (!lemmasIndexes.get(entry.getValue()).contains(fileNumber)) {
+            lemmasIndexes.get(entry.getValue()).add(fileNumber);
+        }
+    }
+
+    private static void writeInfoForEachLemmaToFile (Map<String, List<String>> lemmasMap, String filename){
+        //запись леммы и соответствующей ей информации в файл
+        for (Map.Entry<String, List<String>> entry : lemmasMap.entrySet()){
             StringBuilder result = new StringBuilder(entry.getKey());
             for (String s : entry.getValue()) {
                 result.append(" ").append(s);
             }
-            IOHelper.writeToFileFromNewLine(result.toString(), lemmasPath);
+            IOHelper.writeToFileFromNewLine(result.toString(), filename);
         }
     }
 } 
